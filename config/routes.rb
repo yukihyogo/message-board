97d646f3f31a24bb27fa6f620eeca39e537f3a46
yukihyogo <secondaryshrike@gmail.com>
2015-09-07 15:46:08 +0000

    add _form.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "messages#index"
-  resources :messages , only: [:create]
+#  resources :messages , only: [:create]
+  resources :messages , except: [:index, :new]
 
 #  get 'messages/index'
 
